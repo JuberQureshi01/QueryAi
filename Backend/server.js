bd765596e54529b33298f2c42c30746b08427949
Juber Qureshi <142721071+JuberQureshi01@users.noreply.github.com>
2025-08-07 00:28:02 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,9 @@ const app = express();
 const PORT = 8080;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "https://query-ai-one.vercel.app"
+}));
 // ...
 app.use("/api/auth", authRoutes); // Use auth routes
 
